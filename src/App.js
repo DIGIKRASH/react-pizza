82aacab647d611813dbaad9c1c21c67c47065a49
DIGIKRASH <digitallkrash@gmail.com>
2022-06-26 11:47:20 +0500

    proptypes, axios/fetch, подгрузка из json

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'
+import axios from 'axios'
 
 import { Home, Cart } from './pages'
 import { Header } from './components'
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([])
+
+  React.useEffect(() => {
+    axios.get('http://localhost:3000/db.json').then(({ data }) => {
+      setPizzas(data.pizzas)
+    })
+  }, [])
+
+
   return (
     <div className="wrapper">
 
@@ -12,7 +22,7 @@ function App() {
 
       <div className="content">
         <Routes>
-          <Route exact path='/' element={<Home />} />
+          <Route exact path='/' element={<Home items={pizzas} />} />
           <Route exact path='/cart' element={<Cart />} />
         </Routes>
       </div>
